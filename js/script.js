21fde031f23eb9abf2e2f500e28ca61bbd24a707
unknown <fabiohn20@gmail.com>
2022-06-28 16:18:07 -0300

    Alterações

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function formDados() {
+    console.log(document.getElementById('dado1').value)
+    console.log(document.getElementById('dado2').value)
+    console.log(document.getElementById('dado3').value)
+
+    document.getElementById('dado1').value = ''
+    document.getElementById('dado2').value = ''
+    document.getElementById('dado3').value = ''
+}
