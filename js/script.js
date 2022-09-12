23e15a4e7cfef1da27724d2ed2a2b363240c0232
unknown <fabiohn20@gmail.com>
2022-09-12 16:25:16 -0300

    Correcoes em geral

--- js/script.js
+++ js/script.js
@@ -1,9 +1,46 @@
-function formDados() {
-    console.log(document.getElementById('dado1').value)
-    console.log(document.getElementById('dado2').value)
-    console.log(document.getElementById('dado3').value)
+class ContactForm {
+    constructor(nome, email, desejo) {
+        this.nome = nome;
+        this.email = email;
+        this.desejo = desejo;
+    }
+}
 
-    document.getElementById('dado1').value = ''
-    document.getElementById('dado2').value = ''
-    document.getElementById('dado3').value = ''
+function verificarInfors() {
+    let nome = document.getElementById('nome');
+    let email = document.getElementById('email');
+    let desejo = document.getElementById('desejo');
+
+    let contactForm = new ContactForm(
+        nome.value,
+        email.value,
+        desejo.value
+    );
+
+    console.log(contactForm);
+
+    nome.value = '';
+    email.value = '';
+    desejo.value = '';
+}
+
+let links = document.querySelectorAll('li a[href^="#"]');
+
+links.forEach((l) => {
+    l.addEventListener('click', scrollToClick);
+});
+
+function scrollToClick(event) {
+    event.preventDefault();
+    const to = getScrollTopByHref(event.target);
+
+    window.scroll({
+        top: to,
+        behavior: "smooth"
+    });
+}
+
+function getScrollTopByHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
 }
